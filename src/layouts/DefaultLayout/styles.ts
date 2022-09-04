0a988107502a1b81813c4a1bbd59d76e983692e3
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-09-03 21:21:25 -0300

    first commit

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  display: flex;
+  width: 74rem;
+  height: cal(100vh - 10remm);
+  margin: 5rem auto;
+  padding: 2.5rem;
+
+  background-color: ${(props) => props.theme['gray-800']};
+  border-radius: 8px;
+
+  display: flex;
+  flex-direction: column;
+`
